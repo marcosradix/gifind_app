4d28f18e806ee5838a44a66354a3f403b2ab29a2
Marcos Ferreira <marcosferreira@MacBook-Pro-de-mac.local>
2019-08-06 20:02:40 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Gifind",
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
