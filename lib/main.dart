77097e06332f7504822a04a4cd0f03fd17c81122
Marcos Ferreira <marcosferreira@MacBook-Pro-de-mac.local>
2019-09-16 16:45:53 -0300

    mudança de versão e titulo do app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:gifind_app/ui/homePage.dart';
 void main(){
   runApp(MaterialApp(
     home: HomePage(),
+    title: "GifindApp",
     theme: ThemeData(
       inputDecorationTheme: const InputDecorationTheme(
       enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
